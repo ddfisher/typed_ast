4778be3138f5a9085f89fea470b95965f70b64d2
David Fisher <ddfisher@dropbox.com>
2017-02-12 20:48:18 -0800

    test

--- .travis/build-linux-wheels.sh
+++ .travis/build-linux-wheels.sh
@@ -4,6 +4,11 @@
 for PYBIN in /opt/python/*/bin; do
   if [ $(echo "${PYBIN}" | grep -o '[[:digit:]][[:digit:]]' | head -n 1) -ge 33 ]; then
     # typed_ast only builds on Python 3.3 and newer
-    "${PYBIN}/pip" wheel /io/ -w /io/wheelhouse/
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
   fi
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" -w /io/wheelhouse/
 done
